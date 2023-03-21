d7b8200817fe304856ad3554bae56d309d61a929
the-knd <91854421+The-knd@users.noreply.github.com>
2023-03-20 20:28:42 -0500

    loaded template componenets in template test

---
+++ templates/test.js
@@ -0,0 +1,42 @@
+var head=document.getElementById("header")
+var foot=document.getElementById("footer")
+
+head.innerHTML=''+
+'		<nav class="navbar navbar-expand-lg bg-success fixed-top">'+
+'           <div class="container-fluid">'+
+'               <a class="navbar-brand" href="index.html">Autolegal</a>'+
+'               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
+'                   <span class="navbar-toggler-icon"></span>'+
+'               </button>'+
+'               <div class="collapse navbar-collapse" id="navbarSupportedContent">'+
+'                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">'+
+'                       <li class="nav-item">'+
+'                               <a class="nav-link active" aria-current="page" href="Conocernos.html">Nosotros</a>'+
+'                       </li>'+
+'                       <li class="nav-item dropdown">'+
+'                           <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">'+
+'                                   Rutas y horarios'+
+'                           </a>'+
+'                           <ul class="dropdown-menu">'+
+'                               <li><a class="dropdown-item" href="#">Medellin</a></li>'+
+'                               <li><a class="dropdown-item" href="#">Anserma</a></li>'+
+'                               <li><a class="dropdown-item" href="#">Mariquita</a></li>'+
+'                           </ul>'+
+'                       </li>'+
+'                       <li class="nav-item">'+
+'                               <a class="nav-link active" href="#">Empleados</a>'+
+'                       </li>'+
+'                       <li class="nav-item">'+
+'                               <a class="nav-link active">Contactanos</a>'+
+'                       </li>'+
+'                   </ul>'+
+'                   <div>'+
+'                       <a class="btn btn-light">Registrarse</a>'+
+'                       <a class="btn btn-primary">Iniciar Sesion</a>'+
+'                   </div>'+
+'               </div>'+
+'           </div>'+
+'       </nav>'+
+''
+
+foot.innerHTML="<p>Todos los derechos reservados © Empresa de Transporte</p>"
