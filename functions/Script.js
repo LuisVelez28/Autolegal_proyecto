2555c138883075e3765a047dbad4c5df4e4320a3
* <91854421+The-knd@users.noreply.github.com>
2023-03-21 18:07:21 -0500

    modified to accept init by js, and new template

--- functions/Script.js
+++ functions/Script.js
@@ -1,6 +1,3 @@
-var head=document.getElementById("header")
-var foot=document.getElementById("footer")
-
 function abrirVentanaEmergente() {
 	window.open("", "ventanaEmergente", "width=600,height=400");
 	var ventana = window.open("", "ventanaEmergente");
@@ -10,43 +7,66 @@ function abrirVentanaEmergente() {
     ventana.document.write("<p> El Ministerio de Justicia y del Derecho ha establecido los siguientes canales para que estos derechos puedan ser ejercidos: a) Canal presencial: El ciudadano podrá presentar personalmente su solicitud relacionada con el tratamiento de sus datos personales en el Ministerio de Justicia y del Derecho, ubicado en la Calle 53 # 13-27 de Bogotá (única sede). b) Canal Telefónico: el ciudadano podrá comunicarse a la línea telefónica PBX (+57) (1) 4443100 o línea gratuita nacional 01-800-09-11170. c) Canal Virtual: Página web www.minjusticía.gov.co a través del link http://pqrs.minjusticia.gov.co o al Correo electrónico: servicio.ciudadano@minjusticia.gov.co </p>");
 }
 
-head.innerHTML=''+
-'		<nav class="navbar navbar-expand-lg bg-success fixed-top">'+
-'           <div class="container-fluid">'+
-'               <a class="navbar-brand" href="index.html">Autolegal</a>'+
-'               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
-'                   <span class="navbar-toggler-icon"></span>'+
-'               </button>'+
-'               <div class="collapse navbar-collapse" id="navbarSupportedContent">'+
-'                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">'+
-'                       <li class="nav-item">'+
-'                               <a class="nav-link active" aria-current="page" href="Conocernos.html">Nosotros</a>'+
-'                       </li>'+
-'                       <li class="nav-item dropdown">'+
-'                           <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">'+
-'                                   Rutas y horarios'+
-'                           </a>'+
-'                           <ul class="dropdown-menu">'+
-'                               <li><a class="dropdown-item" href="#">Medellin</a></li>'+
-'                               <li><a class="dropdown-item" href="#">Anserma</a></li>'+
-'                               <li><a class="dropdown-item" href="#">Mariquita</a></li>'+
-'                           </ul>'+
-'                       </li>'+
-'                       <li class="nav-item">'+
-'                               <a class="nav-link active" href="#">Empleados</a>'+
-'                       </li>'+
-'                       <li class="nav-item">'+
-'                               <a class="nav-link active">Contactanos</a>'+
-'                       </li>'+
-'                   </ul>'+
-'                   <div>'+
-'                       <a class="btn btn-light" href="registro.html">Registrarse</a>'+
-'                       <a class="btn btn-primary" href="ingreso.html">Iniciar Sesion</a>'+
-'                   </div>'+
-'               </div>'+
-'           </div>'+
-'       </nav>'+
-''
 
-foot.innerHTML="<p>Todos los derechos reservados © Empresa de Transporte</p>"
+function initTemplate() {
+    let head=document.getElementsByTagName("head")[0]
+    let bootsJs=document.getElementById("boots-js")
+    let header=document.getElementsByTagName("header")[0]
+    let footer=document.getElementsByTagName("footer")[0]
 
+    head.innerHTML=''+
+    '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'+
+    '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
+    '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">'+
+    '<title>Autolegal S.A.S</title>'+
+    '<link rel="stylesheet" type="text/css" href="styles/styles.css">'+
+    ''
+
+    header.innerHTML=''+
+    '		<nav class="navbar navbar-expand-lg bg-success fixed-top">'+
+    '           <div class="container-fluid">'+
+    '               <a class="navbar-brand" href="index.html">Autolegal</a>'+
+    '               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
+    '                   <span class="navbar-toggler-icon"></span>'+
+    '               </button>'+
+    '               <div class="collapse navbar-collapse" id="navbarSupportedContent">'+
+    '                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">'+
+    '                       <li class="nav-item">'+
+    '                               <a class="nav-link active" aria-current="page" href="Conocernos.html">Nosotros</a>'+
+    '                       </li>'+
+    '                       <li class="nav-item dropdown">'+
+    '                           <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">'+
+    '                                   Rutas y horarios'+
+    '                           </a>'+
+    '                           <ul class="dropdown-menu">'+
+    '                               <li><a class="dropdown-item" href="#">Medellin</a></li>'+
+    '                               <li><a class="dropdown-item" href="#">Anserma</a></li>'+
+    '                               <li><a class="dropdown-item" href="#">Mariquita</a></li>'+
+    '                           </ul>'+
+    '                       </li>'+
+    '                       <li class="nav-item">'+
+    '                               <a class="nav-link active" href="#">Empleados</a>'+
+    '                       </li>'+
+    '                       <li class="nav-item">'+
+    '                               <a class="nav-link active">Contactanos</a>'+
+    '                       </li>'+
+    '                   </ul>'+
+    '                   <div>'+
+    '                       <a class="btn btn-light" href="registro.html">Registrarse</a>'+
+    '                       <a class="btn btn-primary" href="ingreso.html">Iniciar Sesion</a>'+
+    '                   </div>'+
+    '               </div>'+
+    '           </div>'+
+    '       </nav>'+
+    ''
+
+    footer.innerHTML="<p>Todos los derechos reservados © Empresa de Transporte</p>"
+
+    bootsJs.setAttribute("src","https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js")
+    bootsJs.setAttribute("integrity","sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN")
+    bootsJs.setAttribute("crossorigin","anonymous")
+
+
+}
+
+initTemplate()
