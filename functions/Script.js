cf888c6a3e1b52ba8a4e04406d27e67bcd4bfd37
* <91854421+The-knd@users.noreply.github.com>
2023-03-22 09:16:55 -0500

    filename to lowercase

--- functions/Script.js
+++ functions/Script.js
@@ -32,7 +32,7 @@ function initTemplate() {
     '               <div class="collapse navbar-collapse" id="navbarSupportedContent">'+
     '                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">'+
     '                       <li class="nav-item">'+
-    '                               <a class="nav-link active" aria-current="page" href="Conocernos.html">Nosotros</a>'+
+    '                               <a class="nav-link active" aria-current="page" href="conocernos.html">Nosotros</a>'+
     '                       </li>'+
     '                       <li class="nav-item dropdown">'+
     '                           <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">'+
--- templates/test.js
+++ templates/test.js
@@ -25,14 +25,14 @@ function initTemplate() {
     header.innerHTML=''+
     '		<nav class="navbar navbar-expand-lg bg-success fixed-top">'+
     '           <div class="container-fluid">'+
-    '               <a class="navbar-brand" href="index.html">Autolegal</a>'+
+    '               <a class="navbar-brand" href="../index.html">Autolegal</a>'+
     '               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
     '                   <span class="navbar-toggler-icon"></span>'+
     '               </button>'+
     '               <div class="collapse navbar-collapse" id="navbarSupportedContent">'+
     '                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">'+
     '                       <li class="nav-item">'+
-    '                               <a class="nav-link active" aria-current="page" href="Conocernos.html">Nosotros</a>'+
+    '                               <a class="nav-link active" aria-current="page" href="../conocernos.html">Nosotros</a>'+
     '                       </li>'+
     '                       <li class="nav-item dropdown">'+
     '                           <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">'+
@@ -52,8 +52,8 @@ function initTemplate() {
     '                       </li>'+
     '                   </ul>'+
     '                   <div>'+
-    '                       <a class="btn btn-light" href="registro.html">Registrarse</a>'+
-    '                       <a class="btn btn-primary" href="ingreso.html">Iniciar Sesion</a>'+
+    '                       <a class="btn btn-light" href="../registro.html">Registrarse</a>'+
+    '                       <a class="btn btn-primary" href="../ingreso.html">Iniciar Sesion</a>'+
     '                   </div>'+
     '               </div>'+
     '           </div>'+
