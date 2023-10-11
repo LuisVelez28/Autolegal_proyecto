917b9a6cec99c1088e5a0fe07e4ed6df7c9847a6
jpinchao <jpinchao@unal.edu.co>
2023-10-11 01:39:03 -0500

    BusYa: Restauracion de Rutas web

--- routes/web.php
+++ routes/web.php
@@ -16,71 +16,51 @@ use App\Http\Controllers\VehiculoController;
 use App\Http\Controllers\ViajeController;
 use App\Models\Vehiculo;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 // traer la vista de la pagina principal que esta en la carpeta views/main/index.blade.php
 
-// Route::middleware(['auth', 'Admin'])->group(function () {
+Route::middleware(['auth', 'Admin'])->group(function () {
+    Route::get('/CuentaAdmin', function () {
+        $vehiculos = Vehiculo::all();
+        return view('cuenta_Admin.indexAdmin', compact('vehiculos'));
+    })->name('CuentaAdmin');
+    Route::resource('/vehiculos', VehiculoController::class);
+    Route::resource('/empleados', EmpleadoController::class);
+    Route::resource('/rutasyhorarios', RutaController::class);
+    Route::resource('/rolUsuario', RolUsuarioController::class);
+    Route::resource('/paradas', ParadaController::class);
+    Route::resource('/tipoEmpleado', TipoEmpleadoController::class);
+    Route::resource('/tipoRuta', TipoRutaController::class);
+    Route::resource('/tipoVehiculo', TipoVehiculoController::class);
+    Route::resource('/viajes', ViajeController::class);
+    Route::resource('/pqrs', PqrsController::class)->except(['create','store']);
+    Route::resource('/usuarios', UserController::class);
+    Route::get('downloadVehiculo-pdf', '\App\Http\controllers\VehiculoController@generar_pdf')->name('descargarVehiculos-pdf');
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+});
 
-//     Route::get('/CuentaAdmin', function () {
-//         $vehiculos = Vehiculo::all();
-//         return view('cuenta_Admin.indexAdmin', compact('vehiculos'));
-//     })->name('CuentaAdmin');
-//     Route::resource('/vehiculos', VehiculoController::class);
-//     Route::resource('/empleados', EmpleadoController::class);
-//     Route::resource('/rutasyhorarios', RutaController::class);
-//     Route::resource('/rolUsuario', RolUsuarioController::class);
-//     Route::resource('/paradas', ParadaController::class);
-//     Route::resource('/tipoEmpleado', TipoEmpleadoController::class);
-//     Route::resource('/tipoRuta', TipoRutaController::class);
-//     Route::resource('/tipoVehiculo', TipoVehiculoController::class);
-//     Route::resource('/viajes', ViajeController::class);
-//     Route::resource('/pqrs', PqrsController::class)->except(['create','store']);
-//     Route::resource('/usuarios', UserController::class);
+Route::middleware(['auth', 'Cliente'])->group(function () {
+    Route::get('/CuentaCliente', function () {
+        return view('indexUsuario');
+    })->name('CuentaCliente');
+});
 
-//     Route::get('downloadVehiculo-pdf', '\App\Http\controllers\VehiculoController@generar_pdf')->name('descargarVehiculos-pdf');
+Route::middleware(['guest'])->group(function () {
+    Route::view('/conocernos', 'conocernos');
+    Route::view('/rutas', 'rutas');
+    Route::resource('/pqrs', PqrsController::class)->only(['store','create']);
+    RouTe::view('/registro', 'registro.registro')->name('registro');
+    Route::view('/ingreso', 'ingreso.ingreso')->name('ingreso');
+    Route::post('/validar-registro', [RegisterController::class, 'store'])->name('validar-registro');
+    Route::post('/iniciar-sesion', [LoginController::class, 'authenticate'])->name('iniciar-sesion');
+});
 
-//     // Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-// });
-
-// Route::middleware(['auth', 'Cliente'])->group(function () {
-
-//     Route::get('/CuentaCliente', function () {
-//         return view('indexUsuario');
-//     })->name('CuentaCliente');
-
-// });
-
-// Route::middleware(['guest'])->group(function () {
-
-//     // Route::view('/conocernos', 'conocernos');
-//     // Route::view('/rutas', 'rutas');
-
-//     Route::resource('/pqrs', PqrsController::class)->only(['store','create']);
-
-//     Route::view('/registro', 'registro.registro')->name('registro');
-//     Route::view('/ingreso', 'ingreso.ingreso')->name('ingreso');
-//     Route::post('/validar-registro', [RegisterController::class, 'store'])->name('validar-registro');
-//     Route::post('/iniciar-sesion', [LoginController::class, 'authenticate'])->name('iniciar-sesion');
-// });
-
-// // No restriccion
-// Route::resource('/contactanos', PqrsController::class);
-// Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-// Route::view('/conocernos','conocernos')->name('conocernos');
-// Route::view('/rutas','rutas')->name('rutas');
-// Route::get('/', function () {
-//     return view('index');
-// })->name('index');
+Route::resource('/contactanos', PqrsController::class);
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::view('/conocernos','conocernos')->name('conocernos');
+Route::view('/rutas','rutas')->name('rutas');
+Route::get('/', function () {
+    return view('index');
+})->name('index');
 
 
 
